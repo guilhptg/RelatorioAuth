4cde3480b0c02306bfbc41e60cee522f97d9f3c8
Guilherme Portugal <flordavidaho@gmail.com>
2025-06-03 13:53:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,41 +5,41 @@ from models import session, Usuario
 
 st.set_page_config(layout='wide')
 
-lista_usuarios = session.query(Usuario).all()
+# lista_usuarios = session.query(Usuario).all()
 
-credenciais = {"usernames": 
-                {
-                    usuario.email: 
-                    {'name': usuario.nome, 
-                    'password': usuario.senha} for usuario in lista_usuarios
-                    }
-                }
+# credenciais = {"usernames": 
+#                 {
+#                     usuario.email: 
+#                     {'name': usuario.nome, 
+#                     'password': usuario.senha} for usuario in lista_usuarios
+#                     }
+#                 }
 
-# Lendo as credenciais do admin do arquivo secrets.toml
+# # Lendo as credenciais do admin do arquivo secrets.toml
 # admin_username = st.secrets["admin"]["username"]
 # admin_password = st.secrets["admin"]["password"]
 
 
 
-authenticator = stauth.Authenticate(credenciais, 'credenciais_hashco', cookie_expiry_days=30)
+# authenticator = stauth.Authenticate(credenciais, 'credenciais_hashco', admin_password, cookie_expiry_days=30)
 
-print(credenciais)
-# login
-def autenticar_usuario(authenticator):
-    nome, authentication_status, username = authenticator.login()
+# print(credenciais)
+# # login
+# def autenticar_usuario(authenticator):
+#     nome, authentication_status, username = authenticator.login()
     
-    if authentication_status:
-        return {'nome': nome, 'username': username}
-    elif authentication_status == False:
-        st.error("Combinação usuário e senha inválidas")
-    else:
-        st.error('Preencha o formulário para fazer login')
+#     if authentication_status:
+#         return {'nome': nome, 'username': username}
+#     elif authentication_status == False:
+#         st.error("Combinação usuário e senha inválidas")
+#     else:
+#         st.error('Preencha o formulário para fazer login')
 
-def logout():
-    authenticator.logout()
+# def logout():
+#     authenticator.logout()
 
 
-dados_usuario = autenticar_usuario(authenticator=authenticator)
+# dados_usuario = autenticar_usuario(authenticator=authenticator)
 
 
 pg = st.navigation(
